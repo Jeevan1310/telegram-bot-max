38d6a9552e5ccb198611e64cdfa36f9733d2c9a2
Jeevan Joseph <jeevanjoseph1310@gmail.com>
2022-08-07 17:46:52 +0530

    added token

--- max.py
+++ max.py
@@ -1,1 +1,14 @@
+from dotenv import load_dotenv
 import json
+import os
+import requests
+
+load_dotenv()
+
+print("Mark 8 is booting up")
+print('Requesting permission to start ----')
+
+API_KEY=os.getenv("TOKEN")
+URL = "https://api.telegram.org/bot{}/".format(API_KEY)
+
+print("Acess granted    --Bot activated----  ")
